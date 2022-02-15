5392f4ac76fbbcd55b8b51274f7027fc4321bb99
Sathyan Elangovan <sathya576@gmail.com>
2022-02-15 15:35:20 +0530

    chnged to applozic

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "KommunicateChatUI-iOS-SDK", url: "https://github.com/Sathyan-Elangovan/KommunicateChatUI-iOS-SDK.git", .revision("a6dcddd3c5b718ec20cb2c0794bbde5c0d4d0810")),
+        .package(name: "ApplozicSwift", url: "https://github.com/AppLozic/ApplozicSwift.git", from: "6.5.0"),
     ],
     targets: [
         .target(
             name: "Kommunicate",
-            dependencies: [.product(name: "KommunicateChatUI-iOS-SDK", package: "KommunicateChatUI-iOS-SDK")],
+            dependencies: [.product(name: "ApplozicSwift", package: "ApplozicSwift")],
             path: "Sources",
             resources: [.process("Resources")]
         ),
